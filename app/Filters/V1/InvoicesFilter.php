65675eff10e5ce24df955f7f6024ecde688a333f
Moataz <moatazsayed119@gmail.com>
2023-05-08 18:11:18 +0300

    first commit

---
+++ app/Filters/V1/InvoicesFilter.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Filters\V1;
+
+use Illuminate\Http\Request;
+use App\Filters\ApiFilter;
+
+class InvoicesFilter extends ApiFilter
+{
+    protected $safeParms = [
+        'customer_id' => ['eq'],
+        'amount' => ['eq', 'lt', 'gt', 'lte', 'gte'],
+        'status' => ['eq', 'neq'],
+        'billed_date' => ['eq', 'lt', 'gt', 'lte', 'gte'],
+        'paid_date' => ['eq', 'lt', 'gt', 'lte', 'gte'],
+    ];
+
+    protected $columnMap= [
+        'customerId' => 'customer_id',
+        'billedDate' => 'billed_date',
+        'paidDate' => 'paid_date',
+    ];
+
+    protected $operatorMap = [
+        'eq' => '=',
+        'lt' => '<',
+        'gt' => '>',
+        'neq' => '!=',
+        'lte' => '<=',
+        'gte' => '>=',
+    ];
+}
